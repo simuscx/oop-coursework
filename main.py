5fec0d4ba0fdc76f110c6b7c29c90a10486b01d2
simuscx <spozniakovas@gmail.com>
2025-04-30 17:55:51 +0300

    minor changes, CharacterSaver renamed to CharacterManager

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from CharacterBuilder import CharacterBuilder
-from Character import CharacterSaver
+from Character import CharacterManager
 from Item import Item
 
 # Builder design pattern usage
@@ -9,22 +9,6 @@ hero = (
     .set_name("m1000")
     .set_class("Wizard")
     .set_stats({"STR": 99, "DEX": 99, "CON": 99, "INT": 99})
-    .set_inventory([Item("itemname", "desc", 999),
-                    Item("itemname2", "desc2", 150)
-    ])
-    .build()
-)
-
-
-# TODO: maybe not a nice formatting above, maybe something like this looks better?
-#  just giving extra lines for bigger definitions like Items or dictionaries
-hero1 = (
-    builder
-    .set_name("m1000")
-    .set_class("Wizard")
-    .set_stats(
-        {"STR": 99, "DEX": 99, "CON": 99, "INT": 99}
-    )
     .set_inventory([
         Item("itemname", "desc", 999),
         Item("itemname2", "desc2", 150)
@@ -37,6 +21,9 @@ bard = (
     .set_name("abcdefg")
     .set_class("Bard")
     .set_stats({"STR": 99})
+    .set_inventory([
+        Item("abcdefg", "abcdefg", 50)
+    ])
     .build()
 )
 
@@ -45,9 +32,9 @@ characters = [hero, bard]
 hero.save_to_file(file_name="hero.json")
 bard.save_to_file(file_name="bard.json")
 
-character_saver = CharacterSaver()
-character_saver.save_characters([hero, bard], "sunday_weekend_event.json")
+character_manager = CharacterManager()
+character_manager.save_characters([hero, bard], "sunday_weekend_event.json")
 
-old_characters = character_saver.load_characters("sunday_weekend_event.json")
+old_characters = character_manager.load_characters("sunday_weekend_event.json")
 for characters in old_characters:
     print(characters)
