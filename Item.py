9908d848c42c4a82ac4909c0cb34541485c07724
simuscx <spozniakovas@gmail.com>
2025-04-30 16:09:29 +0300

    fixed param dockstrings

--- Item.py
+++ Item.py
@@ -4,18 +4,11 @@ class Item:
         Initialize an Item.
 
         :param name: str - The name of the item.
-
-        # TODO: currently used
-        Args: name(Str)
-
-        # TODO: choose one style and stick to it
-
-        Args:
-            :param name: str - The name of the item.
-            :param description: str - A short description of the item.
-            value (int): The item's value in the game's currency.
-            weight (int, optional): The weight of the item. Defaults to 0.
-            rarity (str, optional): The rarity level of the item. Defaults to 'Common'.
+        :param name: str - The name of the item.
+        :param description: str - A short description of the item.
+        :param value: int -The item's value in the game's currency.
+        :param weight: int, optional - The weight of the item. Defaults to 0.
+        :param rarity: str, optional - The rarity level of the item. Defaults to 'Common'
         """
         self.name = name
         self.description = description
@@ -36,8 +29,7 @@ class Item:
         """
         Determine if the item is considered valuable.
 
-        Returns:
-            bool: True if the item's value is greater than 100, False otherwise.
+        return: bool - True if the item's value is greater than 100, False otherwise.
         """
         return self.value > 100
 
@@ -45,8 +37,7 @@ class Item:
         """
         Determine if the item is lightweight.
 
-        Returns:
-            bool: True if the item's weight is less than or equal to 2, False otherwise.
+        return: bool - True if the item's weight is less than or equal to 2, False otherwise.
         """
         return self.weight <= 2
         # TODO: consider random numbers used at the top of the file as random constants for easier change, for example
